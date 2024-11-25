957f9d6ffd7363052df6ae5c6fe8f7c48b9d09d1
Ly <liraedata59@gmail.com>
2024-11-26 02:07:23 +0800

    feat: create note entity

--- Models/Entities/Note.cs
+++ Models/Entities/Note.cs
@@ -1,7 +1,17 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
 namespace SampleCrud_ASPNET.Models.Entities;
 
-
-public class Note
+[Index(nameof(Title), IsUnique = true)]
+public class Note : BaseEntity
 {
+    [ForeignKey("User")]
+    public int UserId { get; set; }
 
+    public string Title { get; set; } = null!;
+    public string Content { get; set; } = null!;
+
+    // Navigation Property
+    public User User { get; set; } = null!;
 }
