afa90634d96a7c5c1ab09e4988b4c04ce2a8863d
Ly <liraedata59@gmail.com>
2024-11-26 02:07:43 +0800

    feat: create token entity

---
+++ Models/Entities/Token.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace SampleCrud_ASPNET.Models.Entities;
+
+[Index(nameof(Key), IsUnique = true)]
+public class Token : BaseEntity
+{
+    [ForeignKey("User")]
+    public int UserId { get; set; }
+
+    public bool IsRevoked { get; set; } = false;
+    public string Key { get; set; } = null!;
+    public DateTime Expiration { get; set; }
+
+    // Navigation Property 
+    public ICollection<User> Users { get; set; } = [];
+}
