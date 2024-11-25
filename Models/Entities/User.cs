acb5c11ab2ca0ccd0b66efdb0f1de06a307bdb41
Ly <liraedata59@gmail.com>
2024-11-26 02:07:02 +0800

    feat: create user entity

--- Models/Entities/User.cs
+++ Models/Entities/User.cs
@@ -1,6 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace SampleCrud_ASPNET.Models.Entities;
 
-public class User
+[Index(nameof(Email), nameof(UserName), IsUnique = true)]
+public class User : BaseEntity
 {
+    public string UserName { get; set; } = null!;
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
 
+    // Navigation Property
+    public ICollection<Note> Notes { get; set; } = [];
+    public ICollection<Token> Tokens { get; set; } = [];
 }
