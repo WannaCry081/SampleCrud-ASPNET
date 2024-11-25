8eaae9b20d6fe566a707cc998aad1dc58b554c0e
Ly <liraedata59@gmail.com>
2024-11-26 03:17:04 +0800

    chore: add 'UpdateContentType' migrations

--- Migrations/20241125191500_UpdateContentType.Designer.cs
+++ Migrations/20241125191500_UpdateContentType.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SampleCrud_ASPNET.Data;
 
@@ -11,9 +12,11 @@ using SampleCrud_ASPNET.Data;
 namespace SampleCrud_ASPNET.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20241125191500_UpdateContentType")]
+    partial class UpdateContentType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20241125191500_UpdateContentType.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SampleCrud_ASPNET.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateContentType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Notes",
+                type: "Text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Notes",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "Text");
+        }
+    }
+}
