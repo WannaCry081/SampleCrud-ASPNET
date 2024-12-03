9beb84ff8bd7315d1c0200cc16cf7a3ebe210b25
Ly <liraedata59@gmail.com>
2024-12-04 00:18:17 +0800

    chore: add 'HashPassword' method in password util

--- Services/Utils/PasswordUtil.cs
+++ Services/Utils/PasswordUtil.cs
@@ -2,5 +2,9 @@ namespace SampleCrud_ASPNET.Services.Utils;
 
 public class PasswordUtil
 {
-    
+    public static string HashPassword(string password)
+    {
+        string hashPassword = BCrypt.Net.BCrypt.HashPassword(password);
+        return hashPassword;
+    }
 }
