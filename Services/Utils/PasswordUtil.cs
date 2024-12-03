4ab5eba00f238befd60a01dbe4fd8fcdbc364eba
Ly <liraedata59@gmail.com>
2024-12-04 00:20:00 +0800

    chore: add 'VerifyPassword' method in password util

--- Services/Utils/PasswordUtil.cs
+++ Services/Utils/PasswordUtil.cs
@@ -7,4 +7,11 @@ public class PasswordUtil
         string hashPassword = BCrypt.Net.BCrypt.HashPassword(password);
         return hashPassword;
     }
+
+    public static bool VerifyPassword(string hashPassword, string password)
+    {
+
+        bool isEqual = BCrypt.Net.BCrypt.Verify(password, hashPassword);
+        return isEqual;
+    }
 }
