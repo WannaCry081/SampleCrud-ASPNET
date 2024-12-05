d4a47000415d1c8d8ba6a8d824cdc3d72c3549ef
Ly <liraedata59@gmail.com>
2024-12-05 23:49:02 +0800

    feat: add register endpoint in auth controller

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,11 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
+using SampleCrud_ASPNET.Controllers.Utils;
+using SampleCrud_ASPNET.Models.Dtos.Auth;
+using SampleCrud_ASPNET.Services.Auth;
 
 namespace SampleCrud_ASPNET.Controllers;
 
 [ApiController]
 [ApiVersion("1.0")]
 [Route("api/v{version:apiVersion}/auth")]
-public class AuthController : ControllerBase
+public class AuthController(
+    ILogger<AuthController> logger,
+    IAuthService authService) : ControllerBase
 {
+    [HttpPost("register")]
+    public async Task<IActionResult> RegisterUser([FromBody] AuthRegisterUserDto authRegisterUser)
+    {
+        try
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ControllerUtil.ValidateState<object>(ModelState));
+            }
 
+            var response = await authService.RegisterUserAsync(authRegisterUser);
+
+            if (!response.Success)
+            {
+                return ControllerUtil.GetErrorActionResult(response);
+            }
+
+            return Ok(response);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "");
+            return Problem();
+        }
+    }
 }
