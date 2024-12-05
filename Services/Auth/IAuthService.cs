9995cb7b0aa7f26a14e39665891d69a040652858
Ly <liraedata59@gmail.com>
2024-12-05 23:48:36 +0800

    chore: add 'RegisterUserAsync' method in auth service

--- Services/Auth/IAuthService.cs
+++ Services/Auth/IAuthService.cs
@@ -1,6 +1,10 @@
+using SampleCrud_ASPNET.Models.Dtos.Auth;
+using SampleCrud_ASPNET.Models.Response;
+
 namespace SampleCrud_ASPNET.Services.Auth;
 
 public interface IAuthService
 {
-
+    Task<ApiResponse<AuthTokenDto>> RegisterUserAsync(
+        AuthRegisterUserDto authRegisterUser);
 }
