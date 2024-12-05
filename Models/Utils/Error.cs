3d318910d7cde524c4137142edffcabf810532c8
Ly <liraedata59@gmail.com>
2024-12-05 23:44:27 +0800

    chore: add 'CREATING_RESOURCE'  method in Error.cs

--- Models/Utils/Error.cs
+++ Models/Utils/Error.cs
@@ -13,4 +13,9 @@ public static class Error
 
     public const string BAD_REQUEST = "Request body is invalid or malformed.";
     public const string VALIDATION_ERROR = "Validation failed on one or more fields.";
+
+    public static string CREATING_RESOURCE(string entity)
+    {
+        return $"Failed to create '{entity}' resource";
+    }
 }
