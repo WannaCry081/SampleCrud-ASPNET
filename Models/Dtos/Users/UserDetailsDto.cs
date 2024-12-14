6fff62564a1da4fe81cd02799179c67229c44205
Ly <liraedata59@gmail.com>
2024-12-14 23:34:53 +0800

    chore: create 'UserDetailsDto' class

---
+++ Models/Dtos/Users/UserDetailsDto.cs
@@ -0,0 +1,9 @@
+namespace SampleCrud_ASPNET.Models.Dtos.Users;
+
+public class UserDetailsDto
+{
+    public string UserName { get; init; } = null!;
+    public string FirstName { get; init; } = null!;
+    public string Lastname { get; init; } = null!;
+    public string Email { get; init; } = null!;
+}
