4c40fcfc1b256e09dc1e2c4b1210d6c649e9fa05
Ly <liraedata59@gmail.com>
2024-12-15 13:17:52 +0800

    chore: create note services method

--- Services/Notes/INoteService.cs
+++ Services/Notes/INoteService.cs
@@ -1,6 +1,13 @@
+using SampleCrud_ASPNET.Models.Dtos.Notes;
+using SampleCrud_ASPNET.Models.Response;
+
 namespace SampleCrud_ASPNET.Services.Notes;
 
 public interface INoteService
 {
-
+    Task<ApiResponse<List<NoteDto>>> GetNotesAsync();
+    Task<ApiResponse<NoteDto>> GetNoteAsync();
+    Task<ApiResponse<NoteDto>> CreateNoteAsync();
+    Task<ApiResponse<NoteDto>> UpdateNoteAsync();
+    Task<ApiResponse<object?>> DeleteNoteAsync();
 }
--- Services/Notes/NoteService.cs
+++ Services/Notes/NoteService.cs
@@ -1,6 +1,32 @@
+using SampleCrud_ASPNET.Models.Dtos.Notes;
+using SampleCrud_ASPNET.Models.Response;
+
 namespace SampleCrud_ASPNET.Services.Notes;
 
 public class NoteService : INoteService
 {
+    public Task<ApiResponse<NoteDto>> CreateNoteAsync()
+    {
+        throw new NotImplementedException();
+    }
 
+    public Task<ApiResponse<object?>> DeleteNoteAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ApiResponse<NoteDto>> GetNoteAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ApiResponse<List<NoteDto>>> GetNotesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ApiResponse<NoteDto>> UpdateNoteAsync()
+    {
+        throw new NotImplementedException();
+    }
 }
