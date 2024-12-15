98a0564fb980fae14c346ea46693a09e4b6c300f
Ly <liraedata59@gmail.com>
2024-12-15 13:16:59 +0800

    chore: create NoteController.cs

--- Controllers/NoteController.cs
+++ Controllers/NoteController.cs
@@ -1,11 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace SampleCrud_ASPNET.Controllers;
 
+[Authorize]
 [ApiController]
 [ApiVersion("1.0")]
 [Route("api/v{version:apiVersion}/notes")]
 public class NoteController : ControllerBase
 {
 
+    [HttpGet]
+    public Task<IActionResult> GetNotes()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("{id}")]
+    public Task<IActionResult> GetNote()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public Task<IActionResult> CreateNote()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("{id}")]
+    public Task<IActionResult> UpdateNote()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("{id}")]
+    public Task<IActionResult> DeleteNote()
+    {
+        throw new NotImplementedException();
+    }
 }
