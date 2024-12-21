47aaa575331cd881b06c7d26e63c389466548f51
Ly <liraedata59@gmail.com>
2024-12-21 13:46:16 +0800

    feat: add XML documentation for 'UsersController' methods and update response types

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Authorization;
 using SampleCrud_ASPNET.Services.Users;
 using SampleCrud_ASPNET.Controllers.Utils;
 using SampleCrud_ASPNET.Models.Utils;
+using SampleCrud_ASPNET.Models.Dtos.Response;
+using SampleCrud_ASPNET.Models.Dtos.Users;
 
 namespace SampleCrud_ASPNET.Controllers;
 
@@ -15,7 +17,25 @@ public class UsersController(
     IUserService userService
     ) : ControllerBase
 {
+    /// <summary>
+    ///     Get user details
+    /// </summary>
+    /// <returns>
+    ///     Returns an <see cref="IActionResult"/> containing:
+    ///     - <see cref="OkObjectResult" /> if the user details are successfully retrieved.
+    ///     - <see cref="UnauthorizedObjectResult" /> if the user is not authenticated.
+    ///     - <see cref="ProblemDetails" /> if an unexpected error occurred.
+    /// </returns>
+    /// <response code="200">Returns the user details.</response>
+    /// <response code="401">Bad request.</response>
+    /// <response code="500">Internal server error.</response>
     [HttpGet("me")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK,
+        Type = typeof(SuccessResponse<UserDetailsDto>))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized,
+        Type = typeof(ErrorResponse))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> GetUserDetails()
     {
         try
